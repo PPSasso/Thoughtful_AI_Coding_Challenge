34f94712ca243b61de939972c6a585073dc42269
PPSasso <pp_sasso@hotmail.com>
2024-08-09 14:26:32 -0300

    Tests

--- tasks.py
+++ tasks.py
@@ -1,16 +1,16 @@
 from robocorp.tasks import task
 from robocorp import workitems
 from classes.ExcelManager import ExcelManager
+import logging
 
 @task
 def workItemsCreation_task():
     # Set the work items
     ExcelManager.setWorkItems(arg_strExcelPath='./input/workitems.xlsx')
-    print()
 
 
 @task
 def main_task():
     for item in workitems.outputs:
-        print(item.payload['search-phrase'])
+        logging.info(item.payload['search-phrase'])
 
