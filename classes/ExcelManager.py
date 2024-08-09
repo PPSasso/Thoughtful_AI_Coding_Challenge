f84f0dbdb3488a65f9ef39e43d1263c1bca6ae70
PPSasso <pp_sasso@hotmail.com>
2024-08-09 09:19:03 -0300

    Basic structure and excel manager - 09/08/2024

---
+++ classes/ExcelManager.py
@@ -0,0 +1,44 @@
+from RPA.Robocorp.WorkItems import WorkItems
+import pandas as pd
+import logging
+
+class ExcelManager:
+    """
+    Class responsible for managing all excel related tasks.
+
+    """
+
+    @classmethod
+    def setWorkItems(cls, arg_strExcelPath:str=None):
+        """
+        Reads the 'workitems.xlsx' file from '\input' and creates a workitem for each row.
+
+        Parameters:
+            arg_strExcelPath (str): The Path of the excel file containing the work items that are to be added to the queue
+        
+        Returns:
+            None
+
+        """
+
+        logging.info("Reading the work items excel file..")
+
+        # Initialize WorkItems instance
+        var_wiWorkItems = WorkItems()
+        var_wiWorkItems.get_input_work_item()
+
+        # Reads the excel file
+        var_dfWorkItems = pd.read_excel(arg_strExcelPath)
+
+        logging.info(f"{len(var_dfWorkItems)} workitems were identified")
+
+        logging.info("Creating new workitems")
+
+        #iterates over the dataframe and adds each row as a new work item
+        for index, row in var_dfWorkItems.iterrows():
+            var_wiNewWorkItem = var_wiWorkItems.create_output_work_item()  # Create a new work item
+            var_wiNewWorkItem.update(row.to_dict())
+            cls.var_wiWorkItems.save_work_item(var_wiNewWorkItem) # Saves the new work item
+
+
+        logging.info("Workitems created successfully!")
--- tasks.py
+++ tasks.py
@@ -1,6 +1,15 @@
 from robocorp.tasks import task
+from classes.ExcelManager import ExcelManager
 
 @task
-def minimal_task():
+def workItemsCreation_task():
+
+    # Set the work items
+    ExcelManager.setWorkItems(arg_strExcelPath='./input/workitems.xlsx')
+
+@task
+def main_task():
     message = "Hello"
     message = message + " World!"
+
+workItemsCreation_task()
